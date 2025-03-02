44731310f77f54dc904550b815316aff24232659
Rushikesh Chaudhari <147311640+Rushikeshchaudhari216@users.noreply.github.com>
2025-03-02 12:34:08 +0530

    Add files via upload

---
+++ 4 Lists and Tuples/Chapter 4/01_list.py
@@ -0,0 +1,7 @@
+friends = ["Apple", "Orange", 5, 35.22, False, "Rushi", "Rahul"]
+
+print(friends[1])
+friends[1]= "Banana"
+
+print(friends[1])
+print(friends[2:5])
---
+++ 4 Lists and Tuples/Chapter 4/04_tuple_methods.py
@@ -0,0 +1,8 @@
+a = (1,425,145,45,342,False,"Rushi","Aditya")
+print(a)
+
+no = a.count(45)
+print(no)
+i = a.index (342)
+print(i)
+print(4 in a)
